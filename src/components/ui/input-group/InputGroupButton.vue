c0ad9fee883c1601c65dd6e84b97189ab5498c2a
Lawal Oyinlola <oyinlolalawal1705@gmail.com>
2026-04-02 20:40:59 +0000

    design: nocturnal navigator - overhaul src/components/ui/input-group/InputGroupButton.vue

---
+++ src/components/ui/input-group/InputGroupButton.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { type HTMLAttributes, computed } from 'vue'
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+import { type InputGroupButtonVariants, inputGroupButtonVariants } from './index'
+
+interface InputGroupButtonProps {
+  variant?: 'ghost' | 'secondary' | 'outline' | 'default' | 'destructive' | 'link'
+  size?: InputGroupButtonVariants['size']
+  class?: HTMLAttributes['class']
+}
+
+const props = defineProps<InputGroupButtonProps>()
+
+const classes = computed(() => {
+  return cn(
+    inputGroupButtonVariants({ size: props.size }),
+    props.class,
+  )
+})
+</script>
+
+<template>
+  <Button
+    :variant="variant"
+    :size="size?.startsWith('icon') ? 'icon' : 'sm'"
+    :class="classes"
+  >
+    <slot />
+  </Button>
+</template>
