64ee210758bccc61eeff03c2a78f9af930e8c707
Lawal Oyinlola <oyinlolalawal1705@gmail.com>
2026-04-08 21:57:22 +0000

    chore: update Checkbox component

--- src/components/ui/checkbox/Checkbox.vue
+++ src/components/ui/checkbox/Checkbox.vue
@@ -32,8 +32,6 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits);
 <template>
   <CheckboxRoot
     v-bind="forwarded"
-    :checked="modelValue"
-    @update:checked="(val) => emits('update:modelValue', val)"
     :class="
       cn(
         'grid place-content-center peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
